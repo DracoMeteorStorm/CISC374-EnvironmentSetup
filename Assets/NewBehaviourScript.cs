f3532d6c983a2526efa42d31d590a89e1348396e
DracoMeteorStorm <adracometeorstorm@gmail.com>
2025-02-09 04:15:03 -0500

    added console messages

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start(
+    )
+    {
+            Debug.Log("there are no good compsci jokes because theres nothing funny about this major");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
